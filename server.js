c6fbd855167347e7e043fbd28332d79923f18896
bittusing <abhilekhk6@gmail.com>
2026-02-13 17:28:41 +0530

    uplode

--- server.js
+++ server.js
@@ -9,8 +9,18 @@ dotenv.config();
 
 const app = express();
 
+// CORS Configuration
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'https://wealthslink.netlify.app'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
