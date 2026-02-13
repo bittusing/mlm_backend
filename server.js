d5cb995cf5e3da930f53b2d81e9c59d0c90a3638
bittusing <abhilekhk6@gmail.com>
2026-02-13 20:15:36 +0530

    uplode

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('trust proxy', 1);
 // SUPER SIMPLE CORS - Allow Everything
 app.use(cors({
   origin: '*',
-  credentials: true,
+//   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
   exposedHeaders: ['Content-Range', 'X-Content-Range']
